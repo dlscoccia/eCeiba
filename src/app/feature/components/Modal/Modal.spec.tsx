6ea11b48b818b2520e9abb0edd317688014a964d
Daniel Lorenzo <dlscoccia@gmail.com>
2021-12-27 19:43:03 -0500

    idk

--- src/app/feature/components/Modal/Modal.spec.tsx
+++ src/app/feature/components/Modal/Modal.spec.tsx
@@ -1,17 +1,7 @@
-import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render } from '@testing-library/react';
-import Modal from './Modal';
 
 test('Renders the component', () => {
-  const modalDiv = document.createElement('div');
-  modalDiv.setAttribute('id', 'root-modal');
-  // get element
-  const component = render(
-    <Modal onClose={() => {}}>
-      <div>Soy el contenido de la modal</div>
-    </Modal>
-  );
-  component.getByText('Soy el contenido de la modal');
-  component.getByText('X');
+  const modalRoot = document.querySelector('#modal-root') as HTMLElement;
+  expect(modalRoot).not.toBeInTheDocument();
 });

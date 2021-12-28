fc277667a791495e5616bf2fd265369fd6d84b3a
Daniel Lorenzo <dlscoccia@gmail.com>
2021-12-28 08:12:11 -0500

    removing extra components

--- src/app/feature/components/Modal/Modal.spec.tsx
+++
@@ -1,7 +0,0 @@
-import '@testing-library/jest-dom/extend-expect';
-import { render } from '@testing-library/react';
-
-test('Renders the component', () => {
-  const modalRoot = document.querySelector('#modal-root') as HTMLElement;
-  expect(modalRoot).not.toBeInTheDocument();
-});
